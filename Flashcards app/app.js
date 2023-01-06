fa4d09bc519d0e8f03ecca8918917aa3729febe0
Cristian M <cristianmicicoi@gmail.com>
2023-01-06 16:57:12 +0200

    continue learning and prracticing JavaScript and creating mini-apps

--- Flashcards app/app.js
+++ Flashcards app/app.js
@@ -1,40 +1,63 @@
+const createCard = document.querySelector('.create-card');
 const addBtn = document.querySelector('.add-btn');
 const delBtn = document.querySelector('.del-btn');
 const saveCard = document.querySelector('.save-card');
 const closeCard = document.querySelector('.close-card');
-const minus = document.querySelector('.minus');
 const inputQ = document.querySelector('#question');
 const inputA = document.querySelector('#answer');
 // const questionToShowAnswer = document.querySelector('.Q');
 // const answer = document.querySelector('.A');
-const cardOutput = document.querySelector('.qa-output');
 let flashcards = []; //am creat o lista globala pentru a salva obiectul flashCard
 
 addBtn.addEventListener('click', function () {
-  console.log(addBtn.value);
+  createCard.classList.remove('hidden');
 });
 
 delBtn.addEventListener('click', function () {
-  console.log(delBtn.value);
+  flashcards = [];
+  renderFlashcards();
 });
 
 saveCard.addEventListener('click', function () {
   // console.log(saveCard.value);
   let flashCard = { question: inputQ.value, answer: inputA.value }; //am creat un obiect pe care il pot salva in lista globala declarata, ca sa fie afisat sub forma asta in consola
-  // inputQ.value = '';
-  // inputA.value = '';
-  console.log(flashcards);
+  flashcards.push(flashCard);
+  renderFlashcards();
+  inputQ.value = '';
+  inputA.value = '';
 });
 
 closeCard.addEventListener('click', function () {
-  console.log(closeCard.value);
+  createCard.classList.add('hidden');
 });
 
-minus.addEventListener('click', function (event) {
-  console.log('minus on-click wipe out the card');
-  event.stopPropagation(); //pentru a nu face click "dublu" atat de pe card si de pe minus
-});
+function renderFlashcards() {
+  let flashcardsContainer = document.querySelector('.flashcards-container');
+  const flashcardsHTML = flashcards
+    .map((element, index) => {
+      return `<div class="flashcard card" id="${index}">
+              <button class="btn flashcard__btn">Delete</button>
+              <hr>
+              <div class="flashcard__question">${element.question}</div>
+              <div class="flashcard__answer hidden">
+                <span>${element.answer}</span>
+              </div>
+            </div>`;
+    })
+    .join('');
 
-cardOutput.addEventListener('click', function () {
-  console.log('This is question-output card!');
-});
+  flashcardsContainer.innerHTML = flashcardsHTML;
+  const flashcardsNode = document.querySelectorAll('.flashcard');
+  flashcardsNode.forEach((flashcard) => {
+    flashcard.addEventListener('click', function () {
+      const flashcardAnswer = flashcard.querySelector('.flashcard__answer');
+      flashcardAnswer.classList.toggle('hidden');
+    });
+    const flashcardButton = flashcard.querySelector('.flashcard__btn');
+    flashcardButton.addEventListener('click', function (event) {
+      event.stopPropagation(); //pentru a nu face click "dublu" atat de pe card si de pe minus
+      flashcards.splice(flashcard.id, 1);
+      renderFlashcards();
+    });
+  });
+}
